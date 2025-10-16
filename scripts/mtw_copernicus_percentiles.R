dbe5f179cb343e33efa1dde4864adcf4ee9ba57e
Ben Tupper <btupper@bigelow.org>
2025-10-16 06:00:16 -0400

    autocommit 2025-10-16 06:00:15

--- scripts/mtw_copernicus_percentiles.R
+++ scripts/mtw_copernicus_percentiles.R
@@ -1,14 +1,18 @@
-# usage: mtw_coperncius_percentiles.R [--] [--help] [--prob PROB] [--n N] [--email EMAIL]
+# usage: mtw_copernicus_percentiles.R [--] [--help] [--prob PROB] [--n N]
+# [--varname VARNAME] [--depth DEPTH] [--region REGION] [--email EMAIL]
 # 
 # Computes N-year daily of p percentiles for each day of year
 # 
 # flags:
-#   -h, --help   show this help message and exit
+#   -h, --help     show this help message and exit
 # 
 # optional arguments:
-#   -p, --prob   probability 0-1 [default: 0.9]
-#   -n, --n      number of recent years to compute percentiles [default: 30]
-#   -e, --email  email to send at end, 'none' to skip [default: none]
+# -p, --prob     probability 0-1 [default: 0.1,0.9]
+# -n, --n        number of recent years to compute percentiles [default: 30]
+# -v, --varname  variable name(s) such as 'thetao' or 'thetao+so' [default: thetao+so]
+# -d, --depth    depth(s) such as 'sur' or 'sur+bot' [default: sur+bot]
+# -r, --region   one regional code ala 'chfc' - just one please [default: chfc]
+# -e, --email    email to send at end, 'none' to skip [default: none]
 
 suppressPackageStartupMessages({
   library(andreas)
@@ -18,23 +22,14 @@ suppressPackageStartupMessages({
   library(charlier)
 })
 
-variable_lut = dplyr::tribble(
-  ~variable, ~name, ~longname
-  "thetao", "temp", "potential temperature",
-  "bottomT", "temp", "bottom temperature",
-  "tob", "temp", "bottom temperature",
-  "sst", "temp", "sea surface temperature",
-  "so", "sal", "salinity",
-  "sob", "sal", "bottom salinity",
-  "sss", "sal", "salinity")
-
 # mthw/chfc/thetao/q10
 # <root>/region/variable/depth/qNN/001.tif
 main = function(probs = c(0.1, 0.9), 
                 N = 30, 
                 region = 'chfc',
-                varnames = c("thetao", "so"),
-                depths = c("sur", "bot") ){
+                varnames = c("temp", "sal"),
+                depths = c("sur", "bot"),
+                lut = variable_lut()){
   charlier::start_logger(file.path(regionpath, "log.txt"))
   charlier::info("varname: %s, depths: %s N: %i, probs: %s", 
                  paste(varnames, collapse = ", "), 
@@ -43,24 +38,26 @@ main = function(probs = c(0.1, 0.9),
                  paste(probs, collapse = ", "))
   r = andreas::read_database(datapath, multiple = TRUE) |>
     dplyr::filter(period == "day",
-                  variable %in% varnames,
+                  name %in% varnames,
                   depth %in% depths,
                   treatment == "raw") |>
     dplyr::filter(date <= as.Date("2023-12-31")) |>
     dplyr::arrange(date) |>
     dplyr::mutate(doy = format(date, "%j")) |>
-    dplyr::group_by(variable, depth, doy) |>
+    dplyr::group_by(name, depth, doy) |>
     dplyr::slice_tail(n = N[1]) |>    # magically it takes the earliest N per doy
                                       # day 366 will be less than N but let's ignore
                                       # that issue for now
     dplyr::group_map(
       function(grp, key){
         charlier::info("varname: %s depth: %s doy: %s", 
-                       key$variable, key$depth, key$doy)
+                       key$name, key$depth, key$doy)
         # mthw/chfc/thetao/q10
         # <root>/region/variable/depth/qNN/001.tif
+        #varname = dplyr::filter(lut, variable == key$variable) |>
+        #  dplyr::pull(name)
         ofile = file.path(regionpath,
-                          key$variable, 
+                          key$name, 
                           key$depth,
                           sprintf("q%0.2i", probs * 100),
                           sprintf("%s.tif", key$doy))
@@ -74,8 +71,8 @@ main = function(probs = c(0.1, 0.9),
         }
         dates = range(grp$date)
         dplyr::tibble(
-          varname = key$variable, 
-          depth = key$depth,
+          varname = key$name[1],
+          depth = key$depth[1],
           doy = key$doy[1],
           first = dates[1],
           last = dates[2],
@@ -103,13 +100,13 @@ Args = arg_parser("Computes N-year daily of p percentiles for each day of year",
                           help = "number of recent years to compute percentiles",
                           default = 30) |>
   argparser::add_argument("--varname",
-                          help = "variable name(s) such as 'thetao' or 'thetao+so'",
-                          default = "thetao+so") |>
+                          help = "variable name(s) such as 'temp' or 'temp+sal'",
+                          default = "temp+sal") |>
   argparser::add_argument("--depth",
                           help = "depth(s) such as 'sur' or 'sur+bot'",
                           default = "sur+bot") |>
   argparser::add_argument("--region",
-                          help = "one regional code ala 'chfc' - just one",
+                          help = "one regional code ala 'chfc' - just one please",
                           default = "chfc") |>
   argparser::add_argument("--email",
                           help = "email to send at end, 'none' to skip",
@@ -125,18 +122,8 @@ N = Args$n
 varnames =  parse_argument(Args$varname)
 depths =  parse_argument(Args$depth)
 
-# premake the output directories
-# mthw/chfc/thetao/q10
-# <root>/region/variable/depth/qNN/001.tif
-varpaths = andreas::copernicus_path("mthw", region, varnames)
-if (!all(dir.exists(varpaths))) ok = sapply(varpaths, dir.create, recursive = TRUE, showWarnings = FALSE)
-#opaths = lapply(varpaths,
-#                function(varpath) andreas::copernicus_path(varpath, sprintf("q%0.2i", prob * 100))) |>
-#  unlist()
-#if (!all(dir.exists(opaths))) ok = lapply(opaths, dir.create, recursive = TRUE, showWarnings = FALSE)
-
 if (!interactive()){
-  ok = main(probs = probs, N = N, varname = varnames, depth = depths, region = region)
+  ok = main(probs = probs, N = N, ds, depth = depths, region = region)
   if (grepl("@", Args$email, fixed = TRUE)){
     charlier::sendmail(to = Args$email,
                        subject = "mtw_copernicus_percentile is done",
